7784081c2cd51b147110069497d59264bbbca83b
devheroo <devhero@yandex.com>
2018-12-14 11:45:25 -0600

    Update uploadandscan.sh

--- uploadandscan.sh
+++ uploadandscan.sh
@@ -5,7 +5,7 @@ TEAM_ANALYSISCENTER_ID=$1
 TEAM_ANALYSISCENTER_KEY=$2
 WRAPPER_LOCATION=$3
 APP_PROFILE_NAME=$4
-INCLUDE_MODULES=$5
 FILEPATH=$5
+INCLUDE_MODULES=$6
 
 /usr/bin/java -jar --add-modules java.se.ee $WRAPPER_LOCATION -vid $TEAM_ANALYSISCENTER_ID -vkey $TEAM_ANALYSISCENTER_KEY -action uploadandscan -appname "${APP_PROFILE_NAME}" -include "${INCLUDE_MODULES} -createprofile true -filepath $FILEPATH -version $DATE
